a6b0d558270c1422b92ff81ed1daf49e98fcd715
JDon492 <joshdonovan492@gmail.com>
2022-06-02 15:47:04 +0100

    Integrated slider to change grid size from value

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+function boardSize(size) {
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = "antiquewhite";
+        board.insertAdjacentElement("beforeend", square);
+    }
+};
+
+
+function gridSlider (input) {
+    boardSize(input);
+}
