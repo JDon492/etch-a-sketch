7545ba99ffbd0c28866370e77fc32226af3337df
JDon492 <joshdonovan492@gmail.com>
2022-06-02 16:51:16 +0100

    Added functional reset buttonto clear the board

--- javascript.js
+++ javascript.js
@@ -2,14 +2,14 @@
 
 function boardSize(size) {
     let board = document.querySelector(".board");
-    let boxes = board.querySelectorAll("box");
+    let boxes = board.querySelectorAll(".box");
     boxes.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < size * size; i++) {
         let square = document.createElement("div");
-        square.classList.add('box'); // Stops users from being able to drag divs.
+        square.classList.add("box"); // Stops users from being able to drag divs.
         square.addEventListener("mousedown", colorSquare);
         square.addEventListener("mouseover", colorSquare);
         square.style.backgroundColor = "antiquewhite";
@@ -29,8 +29,13 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+//Color Functions
+
 function colorSquare(e) {
-    if (e.type === 'mouseover' && !mouseDown) return
+    if (e.type === "mouseover" && !mouseDown) return;
+    if (color === "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
     this.style.backgroundColor = color;
 }
 
@@ -38,3 +43,10 @@ function changeColor(selection) {
     color = selection;
 }
 
+//Reset Function
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let boxes = board.querySelectorAll(".box");
+    boxes.forEach((div) => div.style.backgroundColor = "antiquewhite");
+}
