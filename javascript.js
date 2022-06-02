1ba43de6645bbe7efb951ea37ef9fadfb85b307d
JDon492 <joshdonovan492@gmail.com>
2022-06-02 16:39:09 +0100

    Added color event when mouse down and held. Added RGB color selector

--- javascript.js
+++ javascript.js
@@ -1,16 +1,40 @@
+//Board Size Changer
+
 function boardSize(size) {
     let board = document.querySelector(".board");
+    let boxes = board.querySelectorAll("box");
+    boxes.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < size * size; i++) {
         let square = document.createElement("div");
+        square.classList.add('box'); // Stops users from being able to drag divs.
+        square.addEventListener("mousedown", colorSquare);
+        square.addEventListener("mouseover", colorSquare);
         square.style.backgroundColor = "antiquewhite";
         board.insertAdjacentElement("beforeend", square);
     }
 };
 
+//Grid Slider Input
 
 function gridSlider (input) {
     boardSize(input);
+};
+
+//Mouse Down & Held to Activate colorSquare
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function colorSquare(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    this.style.backgroundColor = color;
 }
+
+function changeColor(selection) {
+    color = selection;
+}
+
